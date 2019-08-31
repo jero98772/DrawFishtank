9ca919add083a18d79084d3a6d6fb018686f58da
jero99 <jero98772@gmail.com>
2019-08-30 20:31:46 -0500

    craedo rama español

--- wwwof_calcular_litros.js
+++ wwwof_calcular_litros.js
@@ -1,56 +1,55 @@
-let teapot;
-var wwwof ='word wide web of fish';
-document.write('<center><p style="font-size:40px">wwwof </p><b><h1>',wwwof,'</h1><b></center>') ;
+var cadena_de_texto='calular volumen de un acuario en litros';
+document.write('<center><p style="font-size:40px">wwwof </p><b><h1>'+"wwwof una parte de un gran proyeto"+'</h1><b></center>') ;
 
-function sumitliters() {
+function calculo() {
 canvas=createCanvas(1000, 1000, WEBGL);
 x = (windowWidth - width) / 2;
 //y = (windowHeight - height) / 2;
 y=250;
 canvas.position(x, y);
-large=document.getElementById("large");
-width=document.getElementById("width");
-deep=document.getElementById("deep");
-cubic=document.getElementById("cubic");
-rectangle=document.getElementById("rectangle");
-Select = document.From2.Select2;
-Select=document.getElementById('Select1');
-selected = Select.options[Select.selectedIndex].value;
-if (selected=='rectangle') {
-	l=large.value
-	w=width.value
-	d=deep.value
-	liters=(l*d*w)/1000;
-	width.disabled=false;
-	deep.disabled=false;
-	console.log(liters)
+largo=document.getElementById("largo");
+ancho=document.getElementById("ancho");
+alto=document.getElementById("alto");
+
+cubico=document.getElementById("cubico");
+rectangulo=document.getElementById("rectangulo");
+
+Selecion=document.From2.Selecion2;
+Selecion=document.getElementById('Selecion1');
+selecionado=Selecion.options[Selecion.selectedIndex].value;
+if (selecionado=='rectangulo') {
+	l=largo.value;
+	a=ancho.value;
+	al=alto.value;
+	litros=(l*a*al)/1000;
+	ancho.disabled=false;
+	alto.disabled=false;
+	console.log(litros);
+	dibujar_el_cubo(l,a,al,0,0);
 	parrafo=document.getElementById('parrafo');
-	parrafo.innerHTML=liters
-	drawcube(l,w,d,0,0);
+	parrafo.innerHTML=litros;
 }
 	
 else {
-	l=large.value
-	liters=(l**3)/1000;
-	width.disabled= true;
-	deep.disabled = true;
-	deep.value=0
-	width.value=0
-	console.log(liters);
+	l=largo.value
+	litros=(l**3)/1000;
+	ancho.disabled= true;
+	alto.disabled = true;
+	alto.value=0;
+	ancho.value=0;
+	console.log(litros);
 	parrafo=document.getElementById('parrafo');
-	parrafo.innerHTML=liters
-	acuarium=drawcube(l);
-	draw=document.getElementById('draw');
-	draw.innerHTML=acuarium;
+	parrafo.innerHTML=litros;
+	acuario=dibujar_el_cubo(l);
 }
 }
 
-function drawcube(l,d,w,x,y) {
-	frameCount;
+function dibujar_el_cubo(l,a,al,x,y) {
+	frameCount= 1.5;
 	background(200);
 	rotateX(frameCount * 0.01);
   	rotateY(frameCount * 0.01);
-	acuarium=box(l,d,w,x,y);
+	acuario=box(l,a,al,x,y);
 //	draw=document.getElementById('draw');
 //	draw.innerHTML=acuarium;
 }
