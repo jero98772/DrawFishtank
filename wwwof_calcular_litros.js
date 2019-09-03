f5263e621e650f210ff318c73ae38467c25443a5
jero99 <jero98772@gmail.com>
2019-09-02 20:31:29 -0500

    actulisando la libreria y añadindo pecera clasica

--- wwwof_calcular_litros.js
+++ wwwof_calcular_litros.js
@@ -1,45 +1,61 @@
 
 var wwwof ='word wide web of fish';
-document.write('<center><p style="font-size:40px">wwwof </p><b><h1>',wwwof,'</h1><b></center>') ;
+document.write('<center><p style="font-size:40px">wwwof </p><b><h2>',wwwof,'</h2><b></center>') ;
 
 function sumitliters() {
-canvas=createCanvas(500, 500, WEBGL);
-x = (windowWidth - width) / 2;
+canvas=createCanvas(350, 350, WEBGL);
+x=650
+//x = (windowWidth - width) / 2;
 //y = (windowHeight - height) / 2;
-y=250;
+y=10;
 canvas.position(x, y);
 large=document.getElementById("large");
 width=document.getElementById("width");
 heigh=document.getElementById("heigh");
 cubic=document.getElementById("cubic");
+parrafo=document.getElementById('parrafo');
 rectangle=document.getElementById("rectangle");
 Select = document.From2.Select2;
 Select=document.getElementById('Select1');
 selected = Select.options[Select.selectedIndex].value;
 if (selected=='rectangle') {
-	l=large.value
-	w=width.value
-	h=heigh.value
+	l=parseFloat(large.value)
+	w=parseFloat(width.value)
+	h=parseFloat(heigh.value)
 	liters=(l*h*w)/1000;
-	width.disabled=false;
-	heigh.disabled=false;
 	console.log(liters)
-	parrafo=document.getElementById('parrafo');
+
 	parrafo.innerHTML=liters
 	acuarium=setInterval(drawcube(l,w,h,0,0),100);
 }
 	
 if (selected=='cubic'){
-	l=large.value
-	liters=(l**3)/1000;
-	width.disabled= true;
-	heigh.disabled = true;
-	heigh.value=0
-	width.value=0
+	h=parseFloat(heigh.value)
+	liters=(h**3)/1000;
+
 	console.log(liters);
-	parrafo=document.getElementById('parrafo');
+
 	parrafo.innerHTML=liters
-	acuarium=setInterval(drawcube(l),100);
+	acuarium=setInterval(drawcube(H),100);
+}
+if (selected=='fishtank_classic'){
+	radius1=parseFloat(large.value);
+	R=radius1;
+	radius2=parseFloat(width.value);
+	r=radius2;
+	h=parseFloat(heigh.value);
+	torusVOL=2*PI*R*(r**2);
+	half_torus=torusVOL/2;
+	cylinder=PI*(R**2)*h;
+	fishtank_classic=cylinder+half_torus
+	liters=fishtank_classic/1000
+	print(torusVOL)
+	print(half_torus)
+	print(cylinder)
+	print(fishtank_classic)
+	print(liters)
+	parrafo.innerHTML=liters
+	draw_fishtank_clasic(R,r,h);
 }
 }
 
@@ -51,5 +67,19 @@ function drawcube(l,w,h,x,y) {
 	acuarium=box(l,w,h,x,y);
 //	draw=document.getElementById('draw');
 //	draw.innerHTML=acuarium;
+}
+function draw_fishtank_clasic(R,r,h){
+	frameCount;
+	background(200);
+	/*
+	rotateX(frameCount * 0.01);
+  	rotateY(frameCount * 0.01);
+	translate(0, 0);*/
+
+	rotateX(frameCount * 0.01);
+  	rotateY(frameCount * 0.01);
+	torus(R,r);
+	cylinder(R, h);
+ 	
 }
 setInterval(sumitliters,100);
